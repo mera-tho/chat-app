3099844a244108429345fecf34990ac1787ca126
mera-tho <dipo1192@yahoo.co.jp>
2020-10-02 15:51:43 +0900

    画像

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+<<<<<<< HEAD
   has_one_attached :image
   validates :content, presence: true, unless: :was_attached?
 
@@ -8,3 +9,8 @@ class Message < ApplicationRecord
     self.image.attached?
   end
 end
+=======
+  validates :content, presence: true
+
+end
+>>>>>>> parent of 5d50530... 修正＋アクティブストレージの導入
